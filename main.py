cf7a4a3f8f16aa57ad8b9bbb41c3223a4c60e40e
Dharani Gowtham <dharaniadithya1998.da@gmail.com>
2023-07-30 05:46:07 +0530

    added requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import bardapi
+from bardapi import Bard
 import streamlit as st
 import os
+import requests
 
 # set your __Secure-1PSID value to key
+os.environ['BARD_API_KEY'] = 'ZAgjbF2rSZndOG-p48Y6qt-7zWBFD4AX9FL5WaGHeaEWPtQPPUUXpPmorUuevfy0OqeTKg.'
 token = 'ZAgjbF2rSZndOG-p48Y6qt-7zWBFD4AX9FL5WaGHeaEWPtQPPUUXpPmorUuevfy0OqeTKg.'
+
+session = requests.Session()
+session.headers = {
+            "Host": "bard.google.com",
+            "X-Same-Domain": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/91.0.4472.114 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "Origin": "https://bard.google.com",
+            "Referer": "https://bard.google.com/",
+        }
+session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY"))
 
 # set your input text
 input_text = st.text_input("Enter the query to search in bard")
@@ -11,7 +25,8 @@ button = st.button("Search")
 # Send an API request and get a response.
 
 if button:
-    response = bardapi.core.Bard(token).get_answer(input_text)
-    st.write(response['content'])
-
-
+    bard = Bard(token=token, session=session, timeout=60)
+    result = bard.get_answer(input_text)['content']
+    # response = bardapi.core.Bard(token).get_answer(input_text)
+    # st.write(response['content'])
+    st.write(result)
